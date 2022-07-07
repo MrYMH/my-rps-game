b4249edfde2a6ebbc0413073d26a6a78b3fd86f2
MrYMH <you@example.com>
2022-07-07 12:59:25 +0200

    first

---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import  React, { useState} from 'react'
+import Modal from './Modal'
+
+
+const Footer = () =>{
+    const[modal , setmodal] = useState(false);
+
+    const toggle=()=>{
+        setmodal(!modal);
+    }
+    return(
+        <div className='footer'>
+            <footer>
+                <div className='text'>
+                    <p>
+                    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"><span>Frontend Mentor</span></a>. 
+                    Coded by <a href="https://www.linkedin.com/in/youssef-hemdan-449543234/" target='_blank'> <span>Youssef Mohamed Hemdan</span></a>.
+                    </p>
+                </div>
+                <button className='rules' onClick={toggle}>
+                    rules
+                </button>
+            </footer>
+            {
+                modal?
+                <Modal toggle={toggle}/>
+                :null
+            }
+        </div>
+    )
+}
+
+export default Footer;
+
+
